5161a7069136573359277caf9adcf5d888a3bf70
Dj.ama1 <dj.ama1@mail.ru>
2022-10-12 10:01:51 +0300

    sitemap added

--- redirect.py
+++ redirect.py
@@ -6,7 +6,9 @@ app = Flask(__name__, template_folder='Templates')
 
 @app.route('/', methods=['post', 'get'])
 def main():
+    render_template('index.html')
     return redirect(request.url.replace('http://', 'https://', 1))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
